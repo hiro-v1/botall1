746d5c8ec3bb36ef91f9d6073dfb1732f280632a
hiro-v1 <mamanteng13@gmail.com>
2025-01-15 19:56:27 +0700

    Update botall.py

--- botall.py
+++ botall.py
@@ -46,13 +46,42 @@ def update_tagall_request_status(request_id, status):
     )
 
 # Fungsi untuk menjalankan tagall
-async def perform_tagall(group_id, message_text, members, duration):
-    if members:
-        tag_message = f"{message_text}\n" + " ".join([f"@{member}" for member in members])
-        await bot.send_message(group_id, tag_message)
-        await asyncio.sleep(duration * 60)  # Durasi dalam menit
-    else:
-        await bot.send_message(group_id, "Tidak ada anggota untuk ditandai.")
+async def tagall_cmd(client, message):
+    msg = await message.reply("silahkan tunggu", quote=True)
+    if client.me.id in tagallgcid and message.chat.id in tagallgcid[client.me.id]:
+        return await msg.edit(
+            "sedang menjalankan perintah silahkan coba lagi nanti atau gunakan perintah <code>batal</code>"
+        )
+    if client.me.id not in tagallgcid:
+        tagallgcid[client.me.id] = set()
+
+    tagallgcid[client.me.id].add(message.chat.id)
+
+    text = message.text.split(None, 1)[1] if len(message.text.split()) != 1 else ""
+    users = [
+    f"[{member.user.first_name}](tg://user?id={member.user.id})"
+        async for member in message.chat.get_members()
+        if not (member.user.is_bot or member.user.is_deleted)
+    ]
+    shuffle(users)
+    m = message.reply_to_message or message
+    await msg.delete()
+    for output in [users[i : i + 5] for i in range(0, len(users), 5)]:
+        if (
+            client.me.id not in tagallgcid
+            or message.chat.id not in tagallgcid[client.me.id]
+        ):
+            break
+        await m.reply(
+            f"{text}\n\n{' '.join(output)}", quote=bool(message.reply_to_message)
+        )
+        await asyncio.sleep(2)
+
+    if client.me.id in tagallgcid and message.chat.id in tagallgcid[client.me.id]:
+        tagallgcid[client.me.id].remove(message.chat.id)
+        if not tagallgcid[client.me.id]:
+            del tagallgcid[client.me.id]
+
 
 # Fungsi untuk melacak anggota
 async def track_members(message):
